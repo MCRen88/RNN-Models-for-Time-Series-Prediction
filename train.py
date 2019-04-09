7d3eb748b77150c56f0453688585315b13a7bb41
Vinit Patel <46385417+vinit97@users.noreply.github.com>
2019-04-09 18:29:59 -0400

    just update

--- train.py
+++ train.py
@@ -2,16 +2,16 @@ import tensorflow as tf
 import data
 import parameters as pa
 
-tf.enable_eager_execution()
 # Just disables the warning, doesn't enable AVX/FMA
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # dataset
-traning_data = data.Dataset(data.CurrencyData(pa.FILE_NAME, pa.DATA_SIZE, pa.OHLC, pa.FEATURE_RANGE), pa.FORECAST_SIZE, pa.WINDOW_SIZE)
-iterator = traning_data.zip_data().make_one_shot_iterator()
+eurusddata = data.TimeSeriesData(pa.FILE_NAME, pa.TRANING_DATA_SIZE, 
+pa.OHLC, pa.FEATURE_RANGE)
+traning_data = data.Dataset(eurusddata.normalized_data(), pa.FORECAST_SIZE,
+ pa.WINDOW_SIZE, pa.FORECAST_SIZE)
 
-#cost  = tf.losses.mean
+iterator = traning_data.zip_data().batch(2).make_one_shot_iterator()
 
-for i in iterator:
-    print(i)
+print(eurusddata.normalized_data().shape)
